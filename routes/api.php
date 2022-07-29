17477d628d0169cab87ec41a5f7a607933933b30
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 12:59:50 +0600

    feat: add member route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\MemberController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,6 +25,8 @@ Route::group([
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('tasks', [TaskController::class, 'index']);
         Route::post('tasks/create', [TaskController::class, 'store']);
+        Route::get('members', [MemberController::class, 'all']);
+        Route::post('members/create', [MemberController::class, 'store']);
     });
     
 });
