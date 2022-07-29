6a8d77b4749903f35bd8b137f59964c408b20fb5
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 09:58:03 +0600

    feat: add login and register

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function login (Request $request) {
+        $login_credentials=[
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ];
+        if(auth()->attempt($login_credentials)){
+            //generate the token for the user
+            $user = auth()->user();
+            $user_login_token= $user->createToken('BS Task Management App')->accessToken;
+            //now return this token on success login attempt
+            return response()->json(['token' => $user_login_token, 'user' => new UserResource($user)], 200);
+        }
+        else{
+            //wrong login credentials, return, user not authorised to our system, return error code 401
+            return response()->json(['error' => 'UnAuthorised Access'], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+     $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+    if ($validator->fails())
+    {
+        return response(['errors'=>$validator->errors()->all()], 422);
+    }
+    $request['password']=Hash::make($request['password']);
+    $request['remember_token'] = Str::random(10);
+    $user = User::create($request->toArray());
+    $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+    $response = ['token' => $token];
+    return response($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+    'middleware' => 'api' 
+], function(){
+    Route::group(['middleware' => 'guest:api'], function(){
+        Route::post('register', [UserController::class, 'register']);
+        Route::post('login', [UserController::class, 'login']);
+    });    
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
