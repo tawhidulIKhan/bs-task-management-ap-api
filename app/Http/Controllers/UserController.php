708e27856f021297edd4e9e7491bc733f9c7cff2
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 13:29:24 +0600

    refactor: change register response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,8 +43,9 @@ class UserController extends Controller
     $request['password']=Hash::make($request['password']);
     $request['remember_token'] = Str::random(10);
     $user = User::create($request->toArray());
-    $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-    $response = ['token' => $token];
+    $response = [
+        'user' =>  $user
+    ];
     return response($response, 200);
     }
 }
