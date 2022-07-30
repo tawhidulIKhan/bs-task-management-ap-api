3912c2a894a9e422b12027748e85d85870e47d47
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 10:36:53 +0600

    add formatter created at date

--- app/Http/Resources/MemberResource.php
+++ app/Http/Resources/MemberResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MemberResource extends JsonResource
@@ -18,7 +19,8 @@ class MemberResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'tasks' => $this->tasks
+            'tasks' => $this->tasks,
+            'created_at' => Carbon::parse($this->created_at)->format('d M, Y - h:m a')
         ];
     }
 }
--- app/Http/Resources/TaskDetailsResource.php
+++ app/Http/Resources/TaskDetailsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskDetailsResource extends JsonResource
@@ -19,7 +20,7 @@ class TaskDetailsResource extends JsonResource
             'title' => $this->title,
             'description' => $this->description,
             'assigned' => $this->assigned,
-            'created_at' => $this->created_at
+            'created_at' => Carbon::parse($this->created_at)->format('d M, Y - h:m a')
         ];
     }
 }
